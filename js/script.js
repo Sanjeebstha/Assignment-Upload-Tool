3c6f66b395718053bc5d81458760f8360852979e
Sanjeeb Shrestha <shrestha2@stud.uni-hannover.de>
2024-12-18 22:08:09 +0100

    all

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 function login() {
     // Redirect to the new page
-    window.location.href = "welcome_page.html"; 
+    window.location.href = "window.html"; 
+}
+
+function join_class() {
+    // Redirect to the new page
+    window.location.href = "class.html"; 
+}
+
+function return_to_page() {
+    // Redirect to the new page
+    window.location.href = "window.html"; 
+}
+
+function logout() {
+    // Clear session or local storage (if any)
+    sessionStorage.clear();
+    
+    // Redirect to the login page
+    window.location.href = "index.html";
 }
